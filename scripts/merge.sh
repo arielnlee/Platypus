87dc29a511084f2b646901b0c5c29108b119b612
arielnlee <103224818+arielnlee@users.noreply.github.com>
2023-08-05 13:16:02 -0700

    update scripts

---
+++ scripts/merge.sh
@@ -0,0 +1,5 @@
+python merge_bigboy.py \
+    --base_model_name_or_path stabilityai/StableBeluga2 \
+    --peft_model_path ./platypus-lora \
+    --output_dir arielnlee/StablePlatypus2-70B \
+    --push_to_hub
